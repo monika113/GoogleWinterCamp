f2cfa43d97e9c2a5157e690008b6606c479f287d
monika113 <sly1103@outlook.com>
2020-01-14 13:57:36 +0000

    minor changes

--- transformer/train_model.sh
+++ transformer/train_model.sh
@@ -1,2 +1,2 @@
-python train.py --epoch 1 --pre_train 1 --load_tokenizer 1 --load_tokenizer_path 'tokenizer/token01' --pre_train_model_path 'model/step01.h5' --save_model_path 'model/step02.h5'
+python train.py --epoch 17 --pre_train 1 --load_tokenizer 1 --load_tokenizer_path 'tokenizer/token01' --pre_train_model_path 'model/step01.h5' --save_model_path 'model/step01.h5'
 

cd1fb965f121413fbcdc34b94031876644b804e7
monika113 <sly1103@outlook.com>
2020-01-14 16:33:01 +0800

    transformer based chatbot

---
+++ transformer/config.py
@@ -0,0 +1,17 @@
+MAX_LENGTH = 40
+
+BATCH_SIZE = 64
+BUFFER_SIZE = 20000
+
+NUM_LAYERS = 2
+D_MODEL = 256
+NUM_HEADS = 8
+UNITS = 512
+DROPOUT = 0.1
+
+EPOCHS = 20
+
+MODEL_PATH = '/model/step01.h5'
+TOKENIZER_PATH = '/tokenizer/token01.txt'
+INPUT_PATH = '../cornell_data/train.enc.pk'
+OUTPUT_PATH = '../cornell_data/train.dec.pk'

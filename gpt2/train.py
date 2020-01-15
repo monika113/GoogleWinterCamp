2e0de094bb12a1f973a19fd360090fa9fde5ae78
luciusssss <zhangchen1999@pku.edu.cn>
2020-01-15 16:59:31 +0800

    Update train.py

--- gpt2/train.py
+++ gpt2/train.py
@@ -174,6 +174,7 @@ def preprocess_que_ans_data(args, tokenizer, n_ctx):
     with open(args.train_tokenized_path, "w", encoding="utf-8") as f:
         for dialogue_index, (ques, ans) in enumerate(zip(data_ques, data_ans)):
             dialogue_ids = [tokenizer.convert_tokens_to_ids('[CLS]')]  # 每个dialogue以[CLS]开头
+            print(ques.split())
             print(tokenizer.tokenize(ques))
             exit()
             dialogue_ids.extend(tokenizer.encode(ques))

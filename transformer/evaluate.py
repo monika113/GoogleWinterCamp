0db805d924b7c0f0f9e14f35a3b18707861c49c5
luciusssss <zhangchen1999@pku.edu.cn>
2020-01-15 21:19:28 +0800

    Update evaluate.py

--- transformer/evaluate.py
+++ transformer/evaluate.py
@@ -157,10 +157,10 @@ if __name__ == "__main__":
         sentence = input('say something: ')
         topk = input('topk')
         topp = input('topp')
-        prediction = self.evaluate(sentence, top_k=int(topk), top_p=int(topp))
+        prediction = bot.evaluate(sentence, top_k=int(topk), top_p=int(topp))
 
-        predicted_sentence = self.tokenizer.decode(
-            [i for i in prediction if i < self.tokenizer.vocab_size])
+        predicted_sentence = bot.tokenizer.decode(
+            [i for i in prediction if i < bot.tokenizer.vocab_size])
 
         print('Input: {}'.format(sentence))
         print('Output: {}'.format(predicted_sentence))

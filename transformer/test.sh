a4fabd5785acd76c6ea15cb89d6d2bd48f4fad6b
monika113 <sly1103@outlook.com>
2020-01-15 10:33:03 +0800

    minor changes

--- transformer/test.sh
+++ transformer/test.sh
@@ -9,7 +9,7 @@ do
   pre_train_model_path=${save_model_path}
   epoch=`expr ${epoch} + ${step}`
   save_model_path=model/step01_${epoch}.h5
-  python train.py --epoch 50 --pre_train 1 --load_tokenizer 1 --load_tokenizer_path 'tokenizer/token01' --pre_train_model_path ${pre_train_model_path} --save_model_path ${save_model_path}
+  python train.py --epoch 50 --pre_train 1 --load_tokenizer 1 --load_tokenizer_path 'tokenizer/token01' --pre_train_model_path ${pre_train_model_path} --save_model_path ${save_model_path} --input_path ${input_path} --output_path ${output_path}
   echo "=============train success, epoch ${epoch}==========="
 done
 
